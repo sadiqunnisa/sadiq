1f26893239a75c3c72add379d555148829fa28fa
sadiqunnisa <sadiqunni@gmail.com>
2023-09-28 11:05:10 +0530

    all code since start of internship

---
+++ javaScript/index.js
@@ -0,0 +1,35 @@
+console.log("HelloWorld");
+var naMe = 10 + "20";
+console.log(naMe);
+console.log(typeof(naMe));
+var first = 9 - "5";
+console.log(first);
+console.log(typeof(first));
+var second = "java" + "script";
+console.log(second);
+console.log(typeof(second));
+var third = "" + "" ;
+console.log(third);
+console.log(typeof(third));
+var fourth = "" + 0 ;
+console.log(fourth);
+console.log(typeof(fourth));
+var fifth = "vinod" - "thapa";
+console.log(fifth);
+console.log(typeof(fifth));
+var sixth = true + true;
+console.log(sixth);
+console.log(typeof(sixth));
+var seven = true + false;
+console.log(seven);
+console.log(typeof(seven));
+var eight = false + true;
+console.log(eight);
+console.log(typeof(eight));
+var nine = false - true;
+console.log(nine);
+console.log(typeof(nine));
+
+
+
+
